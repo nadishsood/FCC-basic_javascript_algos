834faf89847dc8421895c828cf4e09fc80167863
Nadish Sood <nadishsood@gmail.com>
2019-05-31 23:13:49 +0530

    updated with code

---
+++ 2.ReverseAString.js
@@ -0,0 +1,22 @@
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+
+// reverseString("hello") should return a string.
+//         Passed
+// reverseString("hello") should become "olleh".
+//     Passed
+// reverseString("Howdy") should become "ydwoH".
+//     Passed
+// reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+
+
+function reverseString(str) {
+    str = str.split('').reverse().join("")
+    return str;
+}
+
+reverseString
